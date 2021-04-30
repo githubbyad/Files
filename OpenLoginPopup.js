c257f692384d351bf832fa5ef316ede8dfb47df5
Adnan <73897123+githubbyad@users.noreply.github.com>
2021-04-30 23:54:06 +0530

    Create OpenLoginPopup.js

---
+++ OpenLoginPopup.js
@@ -0,0 +1,23 @@
+function OpenLoginPopup(ctl) {
+    var s = readCookie('loginStatus');
+    var sUrlc = 'https://bulletlink.one'; // Server domain
+    var siteU = location.href.match(/:\/\/(.[^/]+)/)[1];
+    var myParameters = new Array();
+    var qrStr;
+    if (ctl == null) {
+        qrStr = location.href;
+    } else {
+        qrStr = ctl.href;
+    }
+    myParameters = ReadSParameters(qrStr);
+    var a = myParameters['a'];
+
+    if (a == '1' && s != 'Yes') {
+        createCookie('curl', escape(qrStr));
+        window.open('/index0.htm?pform={{Login}}&sname=target_form2.asp&site=' + siteU + '&as=' + sUrlc, target = '_parent');
+        return false;
+        //      }
+    } else {
+        return true;
+    }
+}
