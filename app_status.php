5f6d3890187707f32a164a392208292187d04fb0
Adnan <73897123+githubbyad@users.noreply.github.com>
2021-06-09 13:21:14 +0530

    Create app_status.php

---
+++ app_status.php
@@ -0,0 +1,25 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$sites = array("bulletlink.one", "bulletlink.systems", "bulletlink.com", "cnn.com", "facebook.com", "bulletlink.xyz");
+function checkStatus($domain)
+{
+    $curlInit = curl_init($domain);
+    curl_setopt($curlInit, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($curlInit, CURLOPT_HEADER, true);
+    curl_setopt($curlInit, CURLOPT_NOBODY, true);
+    curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curlInit);
+    curl_close($curlInit);
+    if ($response) return true;
+    return false;
+}
+$appSever = '';
+shuffle($sites);
+foreach ($sites as $value) {
+    if (checkStatus('https://' . $value . '/')) {
+        $appSever = 'https://' . $value . '/';
+        break;
+    }
+}
+echo $appSever;
+?>
