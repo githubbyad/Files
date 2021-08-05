d904adeeb3021a3478cc9a5d0ce01481381e2cb2
Adnan <73897123+githubbyad@users.noreply.github.com>
2021-08-05 22:45:05 +0530

    Update get_ip.php

--- PHP/get_ip.php
+++ PHP/get_ip.php
@@ -1,5 +1,10 @@
 <?php
-$ip = file_get_contents('https://api.ipify.org');
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+$ip = isset($_SERVER['HTTP_CF_CONNECTING_IP']) ? $_SERVER['HTTP_CF_CONNECTING_IP'] : $_SERVER['REMOTE_ADDR'];
 $myJSON = '{ "ip":"' . $ip . '"}';
-echo "myFunc(" . $myJSON . ");";
+echo "getVIP(" . $myJSON . ");";
 ?>
