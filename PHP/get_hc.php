dd79ba525b2d1053ee69a6cb2255883ee669ea6a
Adnan <73897123+githubbyad@users.noreply.github.com>
2021-08-07 14:04:01 +0530

    Create get_hc.php

---
+++ PHP/get_hc.php
@@ -0,0 +1,29 @@
+<?php
+//header("Access-Control-Allow-Origin: *");
+if (!isset($_SERVER['HTTP_REFERER'])) {
+    // Page Not Found
+    echo "<script>console.log('Hit Counter Error...');</script>";
+    exit;
+}
+// Add correct path to your countlog.txt file.
+$path = 'hitcounter.txt';
+
+// Opens countlog.txt to read the number of hits.
+$file  = fopen($path, 'r');
+$count = fgets($file, 1000);
+fclose($file);
+
+// Update the count.
+$count = abs(intval($count)) + 1;
+
+// Output the updated count.
+//echo "You are Visitor: {$count}\n";
+$hc = "{$count}";
+$myHC = '{ "hc":"' . $hc . '"}';
+echo "getHC(" . $myHC . ");";
+
+// Opens countlog.txt to change new hit number.
+$file = fopen($path, 'w');
+fwrite($file, $count);
+fclose($file);
+?>
