2335da8723c184eebb4c0a8b6d68a4ad67a43e76
Adnan <73897123+githubbyad@users.noreply.github.com>
2021-08-26 00:56:52 +0530

    Create OpenLoginPopupM2.js

---
+++ JS/OpenLoginPopupM2.js
@@ -0,0 +1,15 @@
+function OpenLoginPopupM(qrStr) {
+    var s = readCookie('loginStatus');
+    var myParameters = new Array();
+    myParameters = ReadSParameters(qrStr);
+    var a = myParameters['a'];
+    var as = readCookie('AppServer');
+    var st = '?site=' + window.location.hostname;
+    if (a == '1' && s != 'Yes') {
+        createCookie('curl', escape(qrStr));
+        window.location = as + 'loginmobile.asp' + st + '&curl=' + escape(qrStr);
+        return false;
+    } else {
+        return true;
+    }
+}
