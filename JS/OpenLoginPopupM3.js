7134a4a2d44b81fef0557fb2dd64d4ffae8f4f98
Adnan <73897123+githubbyad@users.noreply.github.com>
2021-08-26 21:03:42 +0530

    Update and rename OpenLoginPopupM3 to OpenLoginPopupM3.js

--- JS/OpenLoginPopupM3.js
+++ JS/OpenLoginPopupM3.js
@@ -1,13 +1,16 @@
 function OpenLoginPopupM(qrStr) {
     var s = readCookie('loginStatus');
+    var pg = 'a';
     var myParameters = new Array();
     myParameters = ReadSParameters(qrStr);
+    var sg = 'sp';
     var a = myParameters['a'];
     var as = readCookie('AppServer');
     var st = window.location.hostname;
+    var lg = '.' + pg + sg;
     if (a == '1' && s != 'Yes') {
         createCookie('curl', escape(qrStr));
-        window.location = as + 'target_form2.asp?pform={{Login}}&sname=target_form2.asp&site=' + st;
+        window.location = as + 'target_form2' + lg + '?pform={{Login}}&sname=target_form2' + lg + '&site=' + st;
         return false;
     } else {
         return true;
