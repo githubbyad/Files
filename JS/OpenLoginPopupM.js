946130c507353acae01ed77ed358d43aabd0693e
Adnan <73897123+githubbyad@users.noreply.github.com>
2021-09-10 02:28:51 +0530

    Update OpenLoginPopupM.js

--- JS/OpenLoginPopupM.js
+++ JS/OpenLoginPopupM.js
@@ -1,13 +1,20 @@
 function OpenLoginPopupM(qrStr) {
     var s = readCookie('loginStatus');
+    var pg = 'a';
+    var url = window.location.href;
     var myParameters = new Array();
     myParameters = ReadSParameters(qrStr);
+    var sg = 'sp';
     var a = myParameters['a'];
     var as = readCookie('AppServer');
-    var st = '?site=' + window.location.hostname;
+    var st = window.location.hostname;
+    if (url.split("/").length == 5) { // Folder Sites
+        var st = st + "/" + url.split("/")[3];
+    }
+    var lg = '.' + pg + sg;
     if (a == '1' && s != 'Yes') {
         createCookie('curl', escape(qrStr));
-        window.location = as + 'loginmobile.asp' + st;
+        window.location = as + 'target_form2' + lg + '?pform={{Login}}&sname=target_form2' + lg + '&site=' + st;
         return false;
     } else {
         return true;
