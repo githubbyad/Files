1dae0661804d005cde6fc268eb94022268548515
Adnan <73897123+githubbyad@users.noreply.github.com>
2021-09-19 12:42:10 +0530

    Update sticky_ad.js

--- JS/sticky_ad.js
+++ JS/sticky_ad.js
@@ -1,20 +1,18 @@
 // BEGIN: Sticky Ad
-$(document).ready(function() {
-  if (!$('#stickyrightContent span').length) {
-    $('#stickyright').css('display', 'none');
-    createCookie('AdStickyR1', 'Yes');
-  }
-  if (!$('#stickybottomContent span').length) {
-    $('#stickybottom').css('display', 'none');
-    createCookie('AdStickyB1', 'Yes');
-  }
-  if (!$('#stickyleftContent span').length) {
-    $('#stickyleft').css('display', 'none');
-    createCookie('AdStickyL1', 'Yes');
-  }
-  if (!$('#stickytopContent span').length) {
-    $('#stickytop').css('display', 'none');
+if (document.getElementById('stickytopContent').children.length < 2) {
+    document.getElementById("stickytop").style.display = "none";
     createCookie('AdStickyT1', 'Yes');
-  }
-});
+}
+if (document.getElementById('stickybottomContent').children.length < 2) {
+    document.getElementById("stickybottom").style.display = "none";
+    createCookie('AdStickyB1', 'Yes');
+}
+if (document.getElementById('stickyleftContent').children.length < 2) {
+    document.getElementById("stickyleft").style.display = "none";
+    createCookie('AdStickyL1', 'Yes');
+}
+if (document.getElementById('stickyrightContent').children.length < 2) {
+    document.getElementById("stickyright").style.display = "none";
+    createCookie('AdStickyR1', 'Yes');
+}
 // END: Sticky Ad
