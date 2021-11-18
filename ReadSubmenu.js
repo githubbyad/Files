d2eab766d66a4eb0246698c24b9222395e9f7ad4
Adnan <73897123+githubbyad@users.noreply.github.com>
2021-11-18 23:12:23 +0530

    Create ReadSubmenu.js

---
+++ ReadSubmenu.js
@@ -0,0 +1,43 @@
+function ReadSubmenu() {
+    var qrStr = location.href;
+    if (qrStr.indexOf("/index.html") != -1) {
+        return SParameters();
+    }
+    var isFrontPage = qrStr.indexOf('.htm', 0);
+    console.log(isFrontPage);
+    if (isFrontPage == -1) {
+        return SParameters();
+    } else {
+        var sPos, ePos, smenu;
+        sPos = qrStr.indexOf('/index', 0);
+        if (sPos != -1) {
+            ePos = qrStr.lastIndexOf('.', qrStr.length);
+            smenu = qrStr.substring(sPos + 6, ePos);
+            if (smenu == SParameters()) {
+                return SParameters();
+            }
+            if (qrStr.indexOf('smenu') != -1) {
+                var url = new URL(qrStr);
+                var smenup = url.searchParams.get("smenu");
+                return smenup;
+            } else {
+                return smenu;
+            }
+        } else {
+            sPos = qrStr.lastIndexOf('-', qrStr.length);
+            console.log(sPos);
+            if (sPos != -1) {
+                ePos = qrStr.lastIndexOf('.', qrStr.length);
+                smenu = qrStr.substring(sPos + 1, ePos);
+                if (smenu == SParameters()) {
+                    sPos = qrStr.lastIndexOf('-p', qrStr.length);
+                    if (sPos == -1) {
+                        return 'home';
+                    }
+                } else {
+                    return smenu;
+                }
+            }
+        }
+    }
+}
