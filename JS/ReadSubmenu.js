3d96eee4f0fffd3855aa26b36402462d22702344
Adnan <73897123+githubbyad@users.noreply.github.com>
2022-08-29 23:33:57 +0530

    Create ReadSubmenu.js

---
+++ JS/ReadSubmenu.js
@@ -0,0 +1,47 @@
+function ReadSubmenu() {
+    var qrStr = location.href;
+    if (qrStr.indexOf("/hpeditor.") != -1) {
+        var uArr = new URL(qrStr);
+        var uSite = uArr.searchParams.get("site");
+        var uLb = uArr.searchParams.get("lb");
+        qrStr = `https://${uSite}/${uLb}`;
+    }
+    if (qrStr.indexOf("/index.html") != -1) {
+        return SParameters();
+    }
+    var isFrontPage = qrStr.indexOf('.htm', 0);
+    if (isFrontPage == -1) {
+        return SParameters();
+    } else {
+        var sPos, ePos, smenu;
+        sPos = qrStr.indexOf('/index', 0);
+        if (sPos != -1) {
+            ePos = qrStr.lastIndexOf('.', qrStr.length);
+            smenu = qrStr.substring(sPos + 6, ePos);
+            if (smenu == SParameters()) {
+                return SParameters();
+            }
+            if (qrStr.indexOf('smenu') != -1) {
+                var url = new URL(qrStr);
+                var smenup = url.searchParams.get("smenu");
+                return smenup;
+            } else {
+                return smenu;
+            }
+        } else {
+            sPos = qrStr.lastIndexOf('-', qrStr.length);
+            if (sPos != -1) {
+                ePos = qrStr.lastIndexOf('.', qrStr.length);
+                smenu = qrStr.substring(sPos + 1, ePos);
+                if (smenu == SParameters()) {
+                    sPos = qrStr.lastIndexOf('-p', qrStr.length);
+                    if (sPos == -1) {
+                        return 'home';
+                    }
+                } else {
+                    return smenu;
+                }
+            }
+        }
+    }
+}
