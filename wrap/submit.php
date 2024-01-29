ff05dd05e8380a79b8a3429a7c647d1d0800046a
Adnan <73897123+githubbyad@users.noreply.github.com>
2024-01-29 13:01:23 +0530

    Update submit.php

--- wrap/submit.php
+++ wrap/submit.php
@@ -6,16 +6,14 @@ $orders_ins = new Orders;
 
 $settings = new Settings;
 
+// set time zone
 $region = $settings->first()->region;
 date_default_timezone_set($region);
 
-//$data_timestamp = $orders_ins->first(['order_id' => $order_id])->date_timestamp;
+// set time and timestamp
 $order_timestamp = time();
 $order_time = date("D\, jS M h:i:s A", $order_timestamp);
 
-
-// $timeNow = time();
-// $timeNowDate = date("D\, jS M h:i:s A", $timeNow);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
@@ -37,8 +35,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $lastUpdated = 0;
         $lastOrderNumber = $_POST["order_number"];
         $lastOrderDate = $_POST["order_date"];
-        $lastOrderId = $_POST["order_id"];
-        $checkOrdersNumberSql = "SELECT order_number, order_date FROM orders WHERE order_number = '{$lastOrderNumber}' AND order_date  = '{$lastOrderDate}'";
+        //$lastOrderId = $_POST["order_id"];
+        $checkOrdersNumberSql = "SELECT order_number, order_date, order_id FROM orders WHERE order_number = '{$lastOrderNumber}' AND order_date  = '{$lastOrderDate}'";
         $checkOrdersNumberResult = $conn->prepare($checkOrdersNumberSql);
         $checkOrdersNumberResult->execute();
         $checkOrdersNumberFetch = $checkOrdersNumberResult->fetchAll();
@@ -61,15 +59,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     Updated last order.
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>';
-            echo "<p class='response_data update-success d-none' data-timestamp='" . $order_timestamp . "'>" . $order_time . "</p>";
+            echo "<p class='response_data update-success d-none' data-timestamp='" . $order_timestamp . "' data-invoice='".$invoice_number."'>" . $order_time . "</p>";
         }
 
 
         // Inserting into "orders" table values
-        $ordersStatus = 0;
+        $ordersStatus = 0;        
+
         $ordersValue = [
             'order_number' => $_POST["order_number"],
             'order_date' => $order_time,
+            'invoice' => $_POST["invoice"],
             'order_timestamp' => $order_timestamp,
             'staff' => $_POST["staff"],
             'sub_total' => $_POST["sub_total"],
@@ -81,7 +81,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             'customer_repeat' => $_POST["customer_repeat"],
         ];
 
-        $ordersSql = "INSERT INTO orders(order_number,order_date,order_timestamp,staff,sub_total,discount,order_total_amount, order_payment_method, customer_name,customer_phone,customer_repeat) VALUES ";
+
+
+        $ordersSql = "INSERT INTO orders(order_number,order_date,invoice,order_timestamp,staff,sub_total,discount,order_total_amount, order_payment_method, customer_name,customer_phone,customer_repeat) VALUES ";
         $ordersSql .= '("' . implode('","', $ordersValue) . '")';
         $ordersResult = $conn->prepare($ordersSql);
         try {
@@ -132,7 +134,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                 // send date-time to client
 
-                echo "<p class='response_data submit-success d-none' data-timestamp='" . $order_timestamp . "'>" . $order_time . "</p>";
+                echo "<p class='response_data submit-success d-none' data-timestamp='" . $order_timestamp . "' data-invoice='".$invoice_number."'>" . $order_time . "</p>";
             }
         }
 
