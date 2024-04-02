4f842af15469a7249b1adca1562badce83d29734
Adnan <73897123+githubbyad@users.noreply.github.com>
2024-04-02 20:49:15 +0530

    Add files via upload

---
+++ install_ar_customers.bat
@@ -0,0 +1,170 @@
+@echo off
+if x"%1" == x"" Goto Usage
+if x"%2" == x"" Goto Usage
+if x"%3" == x"" Goto Usage
+if x"%4" == x"" Goto Usage
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3837.sql -o Data3837.sql.txt
+echo Batch complete Data3837.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3838.sql -o Data3838.sql.txt
+echo Batch complete Data3838.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3839.sql -o Data3839.sql.txt
+echo Batch complete Data3839.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3840.sql -o Data3840.sql.txt
+echo Batch complete Data3840.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3841.sql -o Data3841.sql.txt
+echo Batch complete Data3841.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3842.sql -o Data3842.sql.txt
+echo Batch complete Data3842.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3843.sql -o Data3843.sql.txt
+echo Batch complete Data3843.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3844.sql -o Data3844.sql.txt
+echo Batch complete Data3844.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3845.sql -o Data3845.sql.txt
+echo Batch complete Data3845.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3846.sql -o Data3846.sql.txt
+echo Batch complete Data3846.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3847.sql -o Data3847.sql.txt
+echo Batch complete Data3847.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3848.sql -o Data3848.sql.txt
+echo Batch complete Data3848.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3849.sql -o Data3849.sql.txt
+echo Batch complete Data3849.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3850.sql -o Data3850.sql.txt
+echo Batch complete Data3850.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3851.sql -o Data3851.sql.txt
+echo Batch complete Data3851.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3852.sql -o Data3852.sql.txt
+echo Batch complete Data3852.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3853.sql -o Data3853.sql.txt
+echo Batch complete Data3853.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3854.sql -o Data3854.sql.txt
+echo Batch complete Data3854.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3855.sql -o Data3855.sql.txt
+echo Batch complete Data3855.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3856.sql -o Data3856.sql.txt
+echo Batch complete Data3856.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3857.sql -o Data3857.sql.txt
+echo Batch complete Data3857.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3858.sql -o Data3858.sql.txt
+echo Batch complete Data3858.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3859.sql -o Data3859.sql.txt
+echo Batch complete Data3859.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3860.sql -o Data3860.sql.txt
+echo Batch complete Data3860.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3861.sql -o Data3861.sql.txt
+echo Batch complete Data3861.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3862.sql -o Data3862.sql.txt
+echo Batch complete Data3862.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3863.sql -o Data3863.sql.txt
+echo Batch complete Data3863.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3864.sql -o Data3864.sql.txt
+echo Batch complete Data3864.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3865.sql -o Data3865.sql.txt
+echo Batch complete Data3865.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3866.sql -o Data3866.sql.txt
+echo Batch complete Data3866.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3867.sql -o Data3867.sql.txt
+echo Batch complete Data3867.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3868.sql -o Data3868.sql.txt
+echo Batch complete Data3868.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3869.sql -o Data3869.sql.txt
+echo Batch complete Data3869.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3870.sql -o Data3870.sql.txt
+echo Batch complete Data3870.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3871.sql -o Data3871.sql.txt
+echo Batch complete Data3871.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3872.sql -o Data3872.sql.txt
+echo Batch complete Data3872.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3873.sql -o Data3873.sql.txt
+echo Batch complete Data3873.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3874.sql -o Data3874.sql.txt
+echo Batch complete Data3874.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3875.sql -o Data3875.sql.txt
+echo Batch complete Data3875.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3876.sql -o Data3876.sql.txt
+echo Batch complete Data3876.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3877.sql -o Data3877.sql.txt
+echo Batch complete Data3877.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3878.sql -o Data3878.sql.txt
+echo Batch complete Data3878.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3879.sql -o Data3879.sql.txt
+echo Batch complete Data3879.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3880.sql -o Data3880.sql.txt
+echo Batch complete Data3880.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3881.sql -o Data3881.sql.txt
+echo Batch complete Data3881.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3882.sql -o Data3882.sql.txt
+echo Batch complete Data3882.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3883.sql -o Data3883.sql.txt
+echo Batch complete Data3883.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3884.sql -o Data3884.sql.txt
+echo Batch complete Data3884.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3885.sql -o Data3885.sql.txt
+echo Batch complete Data3885.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3886.sql -o Data3886.sql.txt
+echo Batch complete Data3886.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3887.sql -o Data3887.sql.txt
+echo Batch complete Data3887.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3888.sql -o Data3888.sql.txt
+echo Batch complete Data3888.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data3889.sql -o Data3889.sql.txt
+echo Batch complete Data3889.sql 
+
+Goto Exit
+:Usage
+echo Usage
+echo Install.bat Server_Name DataBase_Name User_Name Password
+:Exit
