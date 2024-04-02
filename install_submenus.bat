5dc5e49e7fb2e2b374a35f70f22e4e747e6d6b86
Adnan <73897123+githubbyad@users.noreply.github.com>
2024-04-02 20:27:31 +0530

    Add files via upload

---
+++ install_submenus.bat
@@ -0,0 +1,539 @@
+@echo off
+if x"%1" == x"" Goto Usage
+if x"%2" == x"" Goto Usage
+if x"%3" == x"" Goto Usage
+if x"%4" == x"" Goto Usage
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1217.sql -o Data1217.sql.txt
+echo Batch complete Data1217.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1218.sql -o Data1218.sql.txt
+echo Batch complete Data1218.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1219.sql -o Data1219.sql.txt
+echo Batch complete Data1219.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1220.sql -o Data1220.sql.txt
+echo Batch complete Data1220.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1221.sql -o Data1221.sql.txt
+echo Batch complete Data1221.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1222.sql -o Data1222.sql.txt
+echo Batch complete Data1222.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1223.sql -o Data1223.sql.txt
+echo Batch complete Data1223.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1224.sql -o Data1224.sql.txt
+echo Batch complete Data1224.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1225.sql -o Data1225.sql.txt
+echo Batch complete Data1225.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1226.sql -o Data1226.sql.txt
+echo Batch complete Data1226.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1227.sql -o Data1227.sql.txt
+echo Batch complete Data1227.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1228.sql -o Data1228.sql.txt
+echo Batch complete Data1228.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1229.sql -o Data1229.sql.txt
+echo Batch complete Data1229.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1230.sql -o Data1230.sql.txt
+echo Batch complete Data1230.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1231.sql -o Data1231.sql.txt
+echo Batch complete Data1231.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1232.sql -o Data1232.sql.txt
+echo Batch complete Data1232.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1233.sql -o Data1233.sql.txt
+echo Batch complete Data1233.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1234.sql -o Data1234.sql.txt
+echo Batch complete Data1234.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1235.sql -o Data1235.sql.txt
+echo Batch complete Data1235.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1236.sql -o Data1236.sql.txt
+echo Batch complete Data1236.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1237.sql -o Data1237.sql.txt
+echo Batch complete Data1237.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1238.sql -o Data1238.sql.txt
+echo Batch complete Data1238.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1239.sql -o Data1239.sql.txt
+echo Batch complete Data1239.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1240.sql -o Data1240.sql.txt
+echo Batch complete Data1240.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1241.sql -o Data1241.sql.txt
+echo Batch complete Data1241.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1242.sql -o Data1242.sql.txt
+echo Batch complete Data1242.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1243.sql -o Data1243.sql.txt
+echo Batch complete Data1243.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1244.sql -o Data1244.sql.txt
+echo Batch complete Data1244.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1245.sql -o Data1245.sql.txt
+echo Batch complete Data1245.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1246.sql -o Data1246.sql.txt
+echo Batch complete Data1246.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1247.sql -o Data1247.sql.txt
+echo Batch complete Data1247.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1248.sql -o Data1248.sql.txt
+echo Batch complete Data1248.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1249.sql -o Data1249.sql.txt
+echo Batch complete Data1249.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1250.sql -o Data1250.sql.txt
+echo Batch complete Data1250.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1251.sql -o Data1251.sql.txt
+echo Batch complete Data1251.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1252.sql -o Data1252.sql.txt
+echo Batch complete Data1252.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1253.sql -o Data1253.sql.txt
+echo Batch complete Data1253.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1254.sql -o Data1254.sql.txt
+echo Batch complete Data1254.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1255.sql -o Data1255.sql.txt
+echo Batch complete Data1255.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1256.sql -o Data1256.sql.txt
+echo Batch complete Data1256.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1257.sql -o Data1257.sql.txt
+echo Batch complete Data1257.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1258.sql -o Data1258.sql.txt
+echo Batch complete Data1258.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1259.sql -o Data1259.sql.txt
+echo Batch complete Data1259.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1260.sql -o Data1260.sql.txt
+echo Batch complete Data1260.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1261.sql -o Data1261.sql.txt
+echo Batch complete Data1261.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1262.sql -o Data1262.sql.txt
+echo Batch complete Data1262.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1263.sql -o Data1263.sql.txt
+echo Batch complete Data1263.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1264.sql -o Data1264.sql.txt
+echo Batch complete Data1264.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1265.sql -o Data1265.sql.txt
+echo Batch complete Data1265.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1266.sql -o Data1266.sql.txt
+echo Batch complete Data1266.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1267.sql -o Data1267.sql.txt
+echo Batch complete Data1267.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1268.sql -o Data1268.sql.txt
+echo Batch complete Data1268.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1269.sql -o Data1269.sql.txt
+echo Batch complete Data1269.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1270.sql -o Data1270.sql.txt
+echo Batch complete Data1270.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1271.sql -o Data1271.sql.txt
+echo Batch complete Data1271.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1272.sql -o Data1272.sql.txt
+echo Batch complete Data1272.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1273.sql -o Data1273.sql.txt
+echo Batch complete Data1273.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1274.sql -o Data1274.sql.txt
+echo Batch complete Data1274.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1275.sql -o Data1275.sql.txt
+echo Batch complete Data1275.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1276.sql -o Data1276.sql.txt
+echo Batch complete Data1276.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1277.sql -o Data1277.sql.txt
+echo Batch complete Data1277.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1278.sql -o Data1278.sql.txt
+echo Batch complete Data1278.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1279.sql -o Data1279.sql.txt
+echo Batch complete Data1279.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1280.sql -o Data1280.sql.txt
+echo Batch complete Data1280.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1281.sql -o Data1281.sql.txt
+echo Batch complete Data1281.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1282.sql -o Data1282.sql.txt
+echo Batch complete Data1282.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1283.sql -o Data1283.sql.txt
+echo Batch complete Data1283.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1284.sql -o Data1284.sql.txt
+echo Batch complete Data1284.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1285.sql -o Data1285.sql.txt
+echo Batch complete Data1285.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1286.sql -o Data1286.sql.txt
+echo Batch complete Data1286.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1287.sql -o Data1287.sql.txt
+echo Batch complete Data1287.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1288.sql -o Data1288.sql.txt
+echo Batch complete Data1288.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1289.sql -o Data1289.sql.txt
+echo Batch complete Data1289.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1290.sql -o Data1290.sql.txt
+echo Batch complete Data1290.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1291.sql -o Data1291.sql.txt
+echo Batch complete Data1291.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1292.sql -o Data1292.sql.txt
+echo Batch complete Data1292.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1293.sql -o Data1293.sql.txt
+echo Batch complete Data1293.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1294.sql -o Data1294.sql.txt
+echo Batch complete Data1294.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1295.sql -o Data1295.sql.txt
+echo Batch complete Data1295.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1296.sql -o Data1296.sql.txt
+echo Batch complete Data1296.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1297.sql -o Data1297.sql.txt
+echo Batch complete Data1297.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1298.sql -o Data1298.sql.txt
+echo Batch complete Data1298.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1299.sql -o Data1299.sql.txt
+echo Batch complete Data1299.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1300.sql -o Data1300.sql.txt
+echo Batch complete Data1300.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1301.sql -o Data1301.sql.txt
+echo Batch complete Data1301.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1302.sql -o Data1302.sql.txt
+echo Batch complete Data1302.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1303.sql -o Data1303.sql.txt
+echo Batch complete Data1303.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1304.sql -o Data1304.sql.txt
+echo Batch complete Data1304.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1305.sql -o Data1305.sql.txt
+echo Batch complete Data1305.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1306.sql -o Data1306.sql.txt
+echo Batch complete Data1306.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1307.sql -o Data1307.sql.txt
+echo Batch complete Data1307.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1308.sql -o Data1308.sql.txt
+echo Batch complete Data1308.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1309.sql -o Data1309.sql.txt
+echo Batch complete Data1309.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1310.sql -o Data1310.sql.txt
+echo Batch complete Data1310.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1311.sql -o Data1311.sql.txt
+echo Batch complete Data1311.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1312.sql -o Data1312.sql.txt
+echo Batch complete Data1312.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1313.sql -o Data1313.sql.txt
+echo Batch complete Data1313.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1314.sql -o Data1314.sql.txt
+echo Batch complete Data1314.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1315.sql -o Data1315.sql.txt
+echo Batch complete Data1315.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1316.sql -o Data1316.sql.txt
+echo Batch complete Data1316.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1317.sql -o Data1317.sql.txt
+echo Batch complete Data1317.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1318.sql -o Data1318.sql.txt
+echo Batch complete Data1318.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1319.sql -o Data1319.sql.txt
+echo Batch complete Data1319.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1320.sql -o Data1320.sql.txt
+echo Batch complete Data1320.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1321.sql -o Data1321.sql.txt
+echo Batch complete Data1321.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1322.sql -o Data1322.sql.txt
+echo Batch complete Data1322.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1323.sql -o Data1323.sql.txt
+echo Batch complete Data1323.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1324.sql -o Data1324.sql.txt
+echo Batch complete Data1324.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1325.sql -o Data1325.sql.txt
+echo Batch complete Data1325.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1326.sql -o Data1326.sql.txt
+echo Batch complete Data1326.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1327.sql -o Data1327.sql.txt
+echo Batch complete Data1327.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1328.sql -o Data1328.sql.txt
+echo Batch complete Data1328.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1329.sql -o Data1329.sql.txt
+echo Batch complete Data1329.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1330.sql -o Data1330.sql.txt
+echo Batch complete Data1330.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1331.sql -o Data1331.sql.txt
+echo Batch complete Data1331.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1332.sql -o Data1332.sql.txt
+echo Batch complete Data1332.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1333.sql -o Data1333.sql.txt
+echo Batch complete Data1333.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1334.sql -o Data1334.sql.txt
+echo Batch complete Data1334.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1335.sql -o Data1335.sql.txt
+echo Batch complete Data1335.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1336.sql -o Data1336.sql.txt
+echo Batch complete Data1336.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1337.sql -o Data1337.sql.txt
+echo Batch complete Data1337.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1338.sql -o Data1338.sql.txt
+echo Batch complete Data1338.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1339.sql -o Data1339.sql.txt
+echo Batch complete Data1339.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1340.sql -o Data1340.sql.txt
+echo Batch complete Data1340.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1341.sql -o Data1341.sql.txt
+echo Batch complete Data1341.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1342.sql -o Data1342.sql.txt
+echo Batch complete Data1342.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1343.sql -o Data1343.sql.txt
+echo Batch complete Data1343.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1344.sql -o Data1344.sql.txt
+echo Batch complete Data1344.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1345.sql -o Data1345.sql.txt
+echo Batch complete Data1345.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1346.sql -o Data1346.sql.txt
+echo Batch complete Data1346.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1347.sql -o Data1347.sql.txt
+echo Batch complete Data1347.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1348.sql -o Data1348.sql.txt
+echo Batch complete Data1348.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1349.sql -o Data1349.sql.txt
+echo Batch complete Data1349.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1350.sql -o Data1350.sql.txt
+echo Batch complete Data1350.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1351.sql -o Data1351.sql.txt
+echo Batch complete Data1351.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1352.sql -o Data1352.sql.txt
+echo Batch complete Data1352.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1353.sql -o Data1353.sql.txt
+echo Batch complete Data1353.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1354.sql -o Data1354.sql.txt
+echo Batch complete Data1354.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1355.sql -o Data1355.sql.txt
+echo Batch complete Data1355.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1356.sql -o Data1356.sql.txt
+echo Batch complete Data1356.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1357.sql -o Data1357.sql.txt
+echo Batch complete Data1357.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1358.sql -o Data1358.sql.txt
+echo Batch complete Data1358.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1359.sql -o Data1359.sql.txt
+echo Batch complete Data1359.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1360.sql -o Data1360.sql.txt
+echo Batch complete Data1360.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1361.sql -o Data1361.sql.txt
+echo Batch complete Data1361.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1362.sql -o Data1362.sql.txt
+echo Batch complete Data1362.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1363.sql -o Data1363.sql.txt
+echo Batch complete Data1363.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1364.sql -o Data1364.sql.txt
+echo Batch complete Data1364.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1365.sql -o Data1365.sql.txt
+echo Batch complete Data1365.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1366.sql -o Data1366.sql.txt
+echo Batch complete Data1366.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1367.sql -o Data1367.sql.txt
+echo Batch complete Data1367.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1368.sql -o Data1368.sql.txt
+echo Batch complete Data1368.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1369.sql -o Data1369.sql.txt
+echo Batch complete Data1369.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1370.sql -o Data1370.sql.txt
+echo Batch complete Data1370.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1371.sql -o Data1371.sql.txt
+echo Batch complete Data1371.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1372.sql -o Data1372.sql.txt
+echo Batch complete Data1372.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1373.sql -o Data1373.sql.txt
+echo Batch complete Data1373.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1374.sql -o Data1374.sql.txt
+echo Batch complete Data1374.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1375.sql -o Data1375.sql.txt
+echo Batch complete Data1375.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1376.sql -o Data1376.sql.txt
+echo Batch complete Data1376.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1377.sql -o Data1377.sql.txt
+echo Batch complete Data1377.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1378.sql -o Data1378.sql.txt
+echo Batch complete Data1378.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1379.sql -o Data1379.sql.txt
+echo Batch complete Data1379.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1380.sql -o Data1380.sql.txt
+echo Batch complete Data1380.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1381.sql -o Data1381.sql.txt
+echo Batch complete Data1381.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1382.sql -o Data1382.sql.txt
+echo Batch complete Data1382.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1383.sql -o Data1383.sql.txt
+echo Batch complete Data1383.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1384.sql -o Data1384.sql.txt
+echo Batch complete Data1384.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1385.sql -o Data1385.sql.txt
+echo Batch complete Data1385.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1386.sql -o Data1386.sql.txt
+echo Batch complete Data1386.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1387.sql -o Data1387.sql.txt
+echo Batch complete Data1387.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1388.sql -o Data1388.sql.txt
+echo Batch complete Data1388.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1389.sql -o Data1389.sql.txt
+echo Batch complete Data1389.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1390.sql -o Data1390.sql.txt
+echo Batch complete Data1390.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1391.sql -o Data1391.sql.txt
+echo Batch complete Data1391.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1392.sql -o Data1392.sql.txt
+echo Batch complete Data1392.sql 
+
+Goto Exit
+:Usage
+echo Usage
+echo Install.bat Server_Name DataBase_Name User_Name Password
+:Exit
