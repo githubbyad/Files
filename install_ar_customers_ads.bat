edbc4dc07c069704fde0e6e17c19bde9ee8672e6
Adnan <73897123+githubbyad@users.noreply.github.com>
2024-04-02 20:51:56 +0530

    Add files via upload

---
+++ install_ar_customers_ads.bat
@@ -0,0 +1,170 @@
+@echo off
+if x"%1" == x"" Goto Usage
+if x"%2" == x"" Goto Usage
+if x"%3" == x"" Goto Usage
+if x"%4" == x"" Goto Usage
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1097.sql -o Data1097.sql.txt
+echo Batch complete Data1097.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1098.sql -o Data1098.sql.txt
+echo Batch complete Data1098.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1099.sql -o Data1099.sql.txt
+echo Batch complete Data1099.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1100.sql -o Data1100.sql.txt
+echo Batch complete Data1100.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1101.sql -o Data1101.sql.txt
+echo Batch complete Data1101.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1102.sql -o Data1102.sql.txt
+echo Batch complete Data1102.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1103.sql -o Data1103.sql.txt
+echo Batch complete Data1103.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1104.sql -o Data1104.sql.txt
+echo Batch complete Data1104.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1105.sql -o Data1105.sql.txt
+echo Batch complete Data1105.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1106.sql -o Data1106.sql.txt
+echo Batch complete Data1106.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1107.sql -o Data1107.sql.txt
+echo Batch complete Data1107.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1108.sql -o Data1108.sql.txt
+echo Batch complete Data1108.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1109.sql -o Data1109.sql.txt
+echo Batch complete Data1109.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1110.sql -o Data1110.sql.txt
+echo Batch complete Data1110.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1111.sql -o Data1111.sql.txt
+echo Batch complete Data1111.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1112.sql -o Data1112.sql.txt
+echo Batch complete Data1112.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1113.sql -o Data1113.sql.txt
+echo Batch complete Data1113.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1114.sql -o Data1114.sql.txt
+echo Batch complete Data1114.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1115.sql -o Data1115.sql.txt
+echo Batch complete Data1115.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1116.sql -o Data1116.sql.txt
+echo Batch complete Data1116.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1117.sql -o Data1117.sql.txt
+echo Batch complete Data1117.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1118.sql -o Data1118.sql.txt
+echo Batch complete Data1118.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1119.sql -o Data1119.sql.txt
+echo Batch complete Data1119.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1120.sql -o Data1120.sql.txt
+echo Batch complete Data1120.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1121.sql -o Data1121.sql.txt
+echo Batch complete Data1121.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1122.sql -o Data1122.sql.txt
+echo Batch complete Data1122.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1123.sql -o Data1123.sql.txt
+echo Batch complete Data1123.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1124.sql -o Data1124.sql.txt
+echo Batch complete Data1124.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1125.sql -o Data1125.sql.txt
+echo Batch complete Data1125.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1126.sql -o Data1126.sql.txt
+echo Batch complete Data1126.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1127.sql -o Data1127.sql.txt
+echo Batch complete Data1127.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1128.sql -o Data1128.sql.txt
+echo Batch complete Data1128.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1129.sql -o Data1129.sql.txt
+echo Batch complete Data1129.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1130.sql -o Data1130.sql.txt
+echo Batch complete Data1130.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1131.sql -o Data1131.sql.txt
+echo Batch complete Data1131.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1132.sql -o Data1132.sql.txt
+echo Batch complete Data1132.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1133.sql -o Data1133.sql.txt
+echo Batch complete Data1133.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1134.sql -o Data1134.sql.txt
+echo Batch complete Data1134.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1135.sql -o Data1135.sql.txt
+echo Batch complete Data1135.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1136.sql -o Data1136.sql.txt
+echo Batch complete Data1136.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1137.sql -o Data1137.sql.txt
+echo Batch complete Data1137.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1138.sql -o Data1138.sql.txt
+echo Batch complete Data1138.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1139.sql -o Data1139.sql.txt
+echo Batch complete Data1139.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1140.sql -o Data1140.sql.txt
+echo Batch complete Data1140.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1141.sql -o Data1141.sql.txt
+echo Batch complete Data1141.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1142.sql -o Data1142.sql.txt
+echo Batch complete Data1142.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1143.sql -o Data1143.sql.txt
+echo Batch complete Data1143.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1144.sql -o Data1144.sql.txt
+echo Batch complete Data1144.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1145.sql -o Data1145.sql.txt
+echo Batch complete Data1145.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1146.sql -o Data1146.sql.txt
+echo Batch complete Data1146.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1147.sql -o Data1147.sql.txt
+echo Batch complete Data1147.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1148.sql -o Data1148.sql.txt
+echo Batch complete Data1148.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data1149.sql -o Data1149.sql.txt
+echo Batch complete Data1149.sql 
+
+Goto Exit
+:Usage
+echo Usage
+echo Install.bat Server_Name DataBase_Name User_Name Password
+:Exit
