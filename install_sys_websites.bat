9118c70bf07d833cc255e6e6250a3eda8025b9e6
Adnan <73897123+githubbyad@users.noreply.github.com>
2024-04-03 05:10:03 +0530

    Add files via upload

---
+++ install_sys_websites.bat
@@ -0,0 +1,17 @@
+@echo off
+if x"%1" == x"" Goto Usage
+if x"%2" == x"" Goto Usage
+if x"%3" == x"" Goto Usage
+if x"%4" == x"" Goto Usage
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15844.sql -o Data15844.sql.txt
+echo Batch complete Data15844.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15845.sql -o Data15845.sql.txt
+echo Batch complete Data15845.sql 
+
+Goto Exit
+:Usage
+echo Usage
+echo Install.bat Server_Name DataBase_Name User_Name Password
+:Exit
