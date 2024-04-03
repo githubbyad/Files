9430915239ff04baa52833a62f43c968b60821db
Adnan <73897123+githubbyad@users.noreply.github.com>
2024-04-03 06:34:56 +0530

    Add files via upload

---
+++ install_sys_subscribers.bat
@@ -0,0 +1,443 @@
+@echo off
+if x"%1" == x"" Goto Usage
+if x"%2" == x"" Goto Usage
+if x"%3" == x"" Goto Usage
+if x"%4" == x"" Goto Usage
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15523.sql -o Data15523.sql.txt
+echo Batch complete Data15523.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15524.sql -o Data15524.sql.txt
+echo Batch complete Data15524.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15525.sql -o Data15525.sql.txt
+echo Batch complete Data15525.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15526.sql -o Data15526.sql.txt
+echo Batch complete Data15526.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15527.sql -o Data15527.sql.txt
+echo Batch complete Data15527.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15528.sql -o Data15528.sql.txt
+echo Batch complete Data15528.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15529.sql -o Data15529.sql.txt
+echo Batch complete Data15529.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15530.sql -o Data15530.sql.txt
+echo Batch complete Data15530.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15531.sql -o Data15531.sql.txt
+echo Batch complete Data15531.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15532.sql -o Data15532.sql.txt
+echo Batch complete Data15532.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15533.sql -o Data15533.sql.txt
+echo Batch complete Data15533.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15534.sql -o Data15534.sql.txt
+echo Batch complete Data15534.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15535.sql -o Data15535.sql.txt
+echo Batch complete Data15535.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15536.sql -o Data15536.sql.txt
+echo Batch complete Data15536.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15537.sql -o Data15537.sql.txt
+echo Batch complete Data15537.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15538.sql -o Data15538.sql.txt
+echo Batch complete Data15538.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15539.sql -o Data15539.sql.txt
+echo Batch complete Data15539.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15540.sql -o Data15540.sql.txt
+echo Batch complete Data15540.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15541.sql -o Data15541.sql.txt
+echo Batch complete Data15541.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15542.sql -o Data15542.sql.txt
+echo Batch complete Data15542.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15543.sql -o Data15543.sql.txt
+echo Batch complete Data15543.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15544.sql -o Data15544.sql.txt
+echo Batch complete Data15544.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15545.sql -o Data15545.sql.txt
+echo Batch complete Data15545.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15546.sql -o Data15546.sql.txt
+echo Batch complete Data15546.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15547.sql -o Data15547.sql.txt
+echo Batch complete Data15547.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15548.sql -o Data15548.sql.txt
+echo Batch complete Data15548.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15549.sql -o Data15549.sql.txt
+echo Batch complete Data15549.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15550.sql -o Data15550.sql.txt
+echo Batch complete Data15550.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15551.sql -o Data15551.sql.txt
+echo Batch complete Data15551.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15552.sql -o Data15552.sql.txt
+echo Batch complete Data15552.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15553.sql -o Data15553.sql.txt
+echo Batch complete Data15553.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15554.sql -o Data15554.sql.txt
+echo Batch complete Data15554.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15555.sql -o Data15555.sql.txt
+echo Batch complete Data15555.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15556.sql -o Data15556.sql.txt
+echo Batch complete Data15556.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15557.sql -o Data15557.sql.txt
+echo Batch complete Data15557.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15558.sql -o Data15558.sql.txt
+echo Batch complete Data15558.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15559.sql -o Data15559.sql.txt
+echo Batch complete Data15559.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15560.sql -o Data15560.sql.txt
+echo Batch complete Data15560.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15561.sql -o Data15561.sql.txt
+echo Batch complete Data15561.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15562.sql -o Data15562.sql.txt
+echo Batch complete Data15562.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15563.sql -o Data15563.sql.txt
+echo Batch complete Data15563.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15564.sql -o Data15564.sql.txt
+echo Batch complete Data15564.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15565.sql -o Data15565.sql.txt
+echo Batch complete Data15565.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15566.sql -o Data15566.sql.txt
+echo Batch complete Data15566.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15567.sql -o Data15567.sql.txt
+echo Batch complete Data15567.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15568.sql -o Data15568.sql.txt
+echo Batch complete Data15568.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15569.sql -o Data15569.sql.txt
+echo Batch complete Data15569.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15570.sql -o Data15570.sql.txt
+echo Batch complete Data15570.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15571.sql -o Data15571.sql.txt
+echo Batch complete Data15571.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15572.sql -o Data15572.sql.txt
+echo Batch complete Data15572.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15573.sql -o Data15573.sql.txt
+echo Batch complete Data15573.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15574.sql -o Data15574.sql.txt
+echo Batch complete Data15574.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15575.sql -o Data15575.sql.txt
+echo Batch complete Data15575.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15576.sql -o Data15576.sql.txt
+echo Batch complete Data15576.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15577.sql -o Data15577.sql.txt
+echo Batch complete Data15577.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15578.sql -o Data15578.sql.txt
+echo Batch complete Data15578.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15579.sql -o Data15579.sql.txt
+echo Batch complete Data15579.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15580.sql -o Data15580.sql.txt
+echo Batch complete Data15580.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15581.sql -o Data15581.sql.txt
+echo Batch complete Data15581.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15582.sql -o Data15582.sql.txt
+echo Batch complete Data15582.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15583.sql -o Data15583.sql.txt
+echo Batch complete Data15583.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15584.sql -o Data15584.sql.txt
+echo Batch complete Data15584.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15585.sql -o Data15585.sql.txt
+echo Batch complete Data15585.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15586.sql -o Data15586.sql.txt
+echo Batch complete Data15586.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15587.sql -o Data15587.sql.txt
+echo Batch complete Data15587.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15588.sql -o Data15588.sql.txt
+echo Batch complete Data15588.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15589.sql -o Data15589.sql.txt
+echo Batch complete Data15589.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15590.sql -o Data15590.sql.txt
+echo Batch complete Data15590.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15591.sql -o Data15591.sql.txt
+echo Batch complete Data15591.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15592.sql -o Data15592.sql.txt
+echo Batch complete Data15592.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15593.sql -o Data15593.sql.txt
+echo Batch complete Data15593.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15594.sql -o Data15594.sql.txt
+echo Batch complete Data15594.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15595.sql -o Data15595.sql.txt
+echo Batch complete Data15595.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15596.sql -o Data15596.sql.txt
+echo Batch complete Data15596.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15597.sql -o Data15597.sql.txt
+echo Batch complete Data15597.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15598.sql -o Data15598.sql.txt
+echo Batch complete Data15598.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15599.sql -o Data15599.sql.txt
+echo Batch complete Data15599.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15600.sql -o Data15600.sql.txt
+echo Batch complete Data15600.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15601.sql -o Data15601.sql.txt
+echo Batch complete Data15601.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15602.sql -o Data15602.sql.txt
+echo Batch complete Data15602.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15603.sql -o Data15603.sql.txt
+echo Batch complete Data15603.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15604.sql -o Data15604.sql.txt
+echo Batch complete Data15604.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15605.sql -o Data15605.sql.txt
+echo Batch complete Data15605.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15606.sql -o Data15606.sql.txt
+echo Batch complete Data15606.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15607.sql -o Data15607.sql.txt
+echo Batch complete Data15607.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15608.sql -o Data15608.sql.txt
+echo Batch complete Data15608.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15609.sql -o Data15609.sql.txt
+echo Batch complete Data15609.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15610.sql -o Data15610.sql.txt
+echo Batch complete Data15610.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15611.sql -o Data15611.sql.txt
+echo Batch complete Data15611.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15612.sql -o Data15612.sql.txt
+echo Batch complete Data15612.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15613.sql -o Data15613.sql.txt
+echo Batch complete Data15613.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15614.sql -o Data15614.sql.txt
+echo Batch complete Data15614.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15615.sql -o Data15615.sql.txt
+echo Batch complete Data15615.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15616.sql -o Data15616.sql.txt
+echo Batch complete Data15616.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15617.sql -o Data15617.sql.txt
+echo Batch complete Data15617.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15618.sql -o Data15618.sql.txt
+echo Batch complete Data15618.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15619.sql -o Data15619.sql.txt
+echo Batch complete Data15619.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15620.sql -o Data15620.sql.txt
+echo Batch complete Data15620.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15621.sql -o Data15621.sql.txt
+echo Batch complete Data15621.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15622.sql -o Data15622.sql.txt
+echo Batch complete Data15622.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15623.sql -o Data15623.sql.txt
+echo Batch complete Data15623.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15624.sql -o Data15624.sql.txt
+echo Batch complete Data15624.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15625.sql -o Data15625.sql.txt
+echo Batch complete Data15625.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15626.sql -o Data15626.sql.txt
+echo Batch complete Data15626.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15627.sql -o Data15627.sql.txt
+echo Batch complete Data15627.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15628.sql -o Data15628.sql.txt
+echo Batch complete Data15628.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15629.sql -o Data15629.sql.txt
+echo Batch complete Data15629.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15630.sql -o Data15630.sql.txt
+echo Batch complete Data15630.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15631.sql -o Data15631.sql.txt
+echo Batch complete Data15631.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15632.sql -o Data15632.sql.txt
+echo Batch complete Data15632.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15633.sql -o Data15633.sql.txt
+echo Batch complete Data15633.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15634.sql -o Data15634.sql.txt
+echo Batch complete Data15634.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15635.sql -o Data15635.sql.txt
+echo Batch complete Data15635.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15636.sql -o Data15636.sql.txt
+echo Batch complete Data15636.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15637.sql -o Data15637.sql.txt
+echo Batch complete Data15637.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15638.sql -o Data15638.sql.txt
+echo Batch complete Data15638.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15639.sql -o Data15639.sql.txt
+echo Batch complete Data15639.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15640.sql -o Data15640.sql.txt
+echo Batch complete Data15640.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15641.sql -o Data15641.sql.txt
+echo Batch complete Data15641.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15642.sql -o Data15642.sql.txt
+echo Batch complete Data15642.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15643.sql -o Data15643.sql.txt
+echo Batch complete Data15643.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15644.sql -o Data15644.sql.txt
+echo Batch complete Data15644.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15645.sql -o Data15645.sql.txt
+echo Batch complete Data15645.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15646.sql -o Data15646.sql.txt
+echo Batch complete Data15646.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15647.sql -o Data15647.sql.txt
+echo Batch complete Data15647.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15648.sql -o Data15648.sql.txt
+echo Batch complete Data15648.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15649.sql -o Data15649.sql.txt
+echo Batch complete Data15649.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15650.sql -o Data15650.sql.txt
+echo Batch complete Data15650.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15651.sql -o Data15651.sql.txt
+echo Batch complete Data15651.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15652.sql -o Data15652.sql.txt
+echo Batch complete Data15652.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15653.sql -o Data15653.sql.txt
+echo Batch complete Data15653.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15654.sql -o Data15654.sql.txt
+echo Batch complete Data15654.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15655.sql -o Data15655.sql.txt
+echo Batch complete Data15655.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15656.sql -o Data15656.sql.txt
+echo Batch complete Data15656.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15657.sql -o Data15657.sql.txt
+echo Batch complete Data15657.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15658.sql -o Data15658.sql.txt
+echo Batch complete Data15658.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15659.sql -o Data15659.sql.txt
+echo Batch complete Data15659.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15660.sql -o Data15660.sql.txt
+echo Batch complete Data15660.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15661.sql -o Data15661.sql.txt
+echo Batch complete Data15661.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15662.sql -o Data15662.sql.txt
+echo Batch complete Data15662.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15663.sql -o Data15663.sql.txt
+echo Batch complete Data15663.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15664.sql -o Data15664.sql.txt
+echo Batch complete Data15664.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15665.sql -o Data15665.sql.txt
+echo Batch complete Data15665.sql 
+
+sqlcmd -S %1 -d %2 -U %3 -P %4 -i Data15666.sql -o Data15666.sql.txt
+echo Batch complete Data15666.sql 
+
+Goto Exit
+:Usage
+echo Usage
+echo Install.bat Server_Name DataBase_Name User_Name Password
+:Exit
