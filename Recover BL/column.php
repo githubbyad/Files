6a9f7d8326b13bcd2c1b15d21a94bd211fc3a784
Adnan <73897123+githubbyad@users.noreply.github.com>
2024-04-03 12:11:20 +0530

    Update column.php

--- Recover BL/column.php
+++ Recover BL/column.php
@@ -1,1 +1,38 @@
-t
+<?php
+
+// Define the directory where your files are located
+$directory = 'files/other/';
+
+// Open the directory
+if ($handle = opendir($directory)) {
+    // Loop through each file in the directory
+    while (false !== ($file = readdir($handle))) {
+        // Exclude current directory (.) and parent directory (..)
+        if ($file != "." && $file != "..") {
+            // Read the contents of the file
+            $filePath = $directory . $file;
+            $contents = file_get_contents($filePath);
+
+            //$pattern = '/^\[\{(.+?)\}\]$/';      
+            $pattern = '/\[{(.*?)}\]/';
+
+            // for replacing column name from [{XXX}] to [xyz]
+            $newContents = preg_replace_callback($pattern, function ($matches) {
+                // $matches[0] contains the entire match. $matches[1] contains the content inside [{ and }]
+                $match = $matches[1];
+
+                // Perform replacement logic here                
+                $replacement = str_replace($match, '[xyz]', $match);
+
+                // Return the replacement string
+                return $replacement;
+            }, $contents);
+
+            file_put_contents($filePath, $newContents, LOCK_EX, null);
+
+            echo "Success: {$filePath}<br>";
+        }
+    }
+    // Close the directory handle
+    closedir($handle);
+}
