614100ee611216edfe6fae5ed5c1bcd71c7517a2
Adnan <73897123+githubbyad@users.noreply.github.com>
2024-04-04 18:32:55 +0530

    Update submit_hide.js

--- submit_hide.js
+++ submit_hide.js
@@ -2,15 +2,18 @@
 if (window.location.href.includes('&pform=pages&pkeyname=sys_information_id')) {
     if (document.querySelector('#issue_date')) {
         let issue = document.querySelector('#issue_date').value;
+        let body = document.querySelector('#issue_date').value;
         let form = document.querySelector(".my-form");
-        let issueDate = new Date(issue); 
-        let stopDate = new Date("2024-03-28"); 
-        if (issueDate <= stopDate) {
-            console.log("Form submission prevented!");
-            form.addEventListener("submit", function (event) {
-                event.preventDefault();
-                console.log("Form submission will not work!");
-            });
+        let issueDate = new Date(issue);
+        let stopDate = new Date("2024-03-28");
+        if (body == "") {
+            if (issueDate <= stopDate) {
+                console.log("Form submission prevented!");
+                form.addEventListener("submit", function (event) {
+                    event.preventDefault();
+                    alert("Form Submission Disabled for Blank Articles")
+                });
+            }
         }
     }
 }
