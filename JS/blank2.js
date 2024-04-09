1278b0e8d37538426bcd7a648e0e846ce4a0bf80
Adnan <73897123+githubbyad@users.noreply.github.com>
2024-04-09 21:20:47 +0530

    Update and rename blank.js to blank2.js

--- JS/blank2.js
+++ JS/blank2.js
@@ -8,13 +8,14 @@
                     let form = document.querySelector(".my-form");
                     let issueDate = new Date(issue);
                     let stopDate = new Date("2024-03-28");
+                    let art_url = window.location.href.replace("/form2.","/view_file.");
                     if (body == "") {
                         if (issueDate <= stopDate) {  
                             //form.setAttribute('onsubmit', '');                          
                             form.addEventListener("submit", function (event) {
                                 if (!window.location.href.includes('&subx=Y')) {
                                     //event.preventDefault();
-                                    alert(`The Article body is currently blank. <br><br>To proceed, please click on the "View Article" link below, then copy the content of the article body and paste it into the designated "Body" field.<br><br> <a href="https://google.com" target="_blank" style="text-decoration: underline !important;">View Article</a><br><br>`);
+                                    alert(`The Article body is currently blank. <br><br>To proceed, please click on the "View Article" link below, then copy the content of the article body and paste it into the designated "Body" field.<br><br> <a href="${art_url}" target="_blank" style="text-decoration: underline !important;">View Article</a><br><br>`);
                                 }
                             });
                         }
