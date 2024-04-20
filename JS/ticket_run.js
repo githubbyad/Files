fc165327b411126ec81d5c94aad80f935fc6a95b
Adnan <73897123+githubbyad@users.noreply.github.com>
2024-04-21 00:25:53 +0530

    Create ticket_run.js

---
+++ JS/ticket_run.js
@@ -0,0 +1,2 @@
+var staff = "Usman";
+document.body.appendChild(Object.assign(document.createElement('script'), {src: 'https://bulletlink.com/ticket.js?staff=' + staff + '&v=' + (Date.now() * 1000)}));
