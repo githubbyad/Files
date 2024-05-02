8b652f0920de79628dba1b1d7c5c1d0a313952ab
Adnan <73897123+githubbyad@users.noreply.github.com>
2024-05-02 15:52:50 +0530

    Update FileValidator.js

--- JS/FileValidator.js
+++ JS/FileValidator.js
@@ -2,13 +2,11 @@ function FileValidator(myKey, myControl, fileType) {
     var myValue = trim(myControl.value);
     var reFile;
     if (fileType == 'All') {
-        reFile = /^.+\.(([jJ][pP][eE]?[gG])|([gG][iI][fF])|([iI][cC][oO])|([jJ][sS])|([pP][nN][gG])|([cC][sS][sS])|([wW][mM][vV])|([wW][mM][aA])|([pP][dD][fF])|([sS][wW][fF])|([mM][pP][3])|([mM][pP][4])|([rR][sS][sS])|([tT][xX][tT])|([mM][pP][gG])|([mM][pP][eE][gG])|([aA][vV][iI])|([wW][aA][vV])|([aA][iI][fF])|([aA][iI][fF][fF])|([mM][oO][vV])|([hH][tT][mM])|([hH][tT][mM][lL])|([fF][lL][aA])|([xX][mM][lL])|([jJ][sS][oO][nN]))$/;
+        reFile = /^.+\.(([jJ][pP][eE]?[gG])|([gG][iI][fF])|([iI][cC][oO])|([jJ][sS])|([pP][nN][gG])|([cC][sS][sS])|([wW][mM][vV])|([wW][mM][aA])|([pP][dD][fF])|([sS][wW][fF])|([wW][eE][bB][pP])|([mM][pP][3])|([mM][pP][4])|([rR][sS][sS])|([tT][xX][tT])|([mM][pP][gG])|([mM][pP][eE][gG])|([aA][vV][iI])|([wW][aA][vV])|([aA][iI][fF])|([aA][iI][fF][fF])|([mM][oO][vV])|([hH][tT][mM])|([hH][tT][mM][lL])|([fF][lL][aA])|([xX][mM][lL])|([jJ][sS][oO][nN]))$/;
     }
-    if (fileType == 'Image') {
-        reFile = /^.+\.(([jJ][pP][eE]?[gG])|([gG][iI][fF])|([iI][cC][oO])|([pP][dD][fF])|([pP][nN][gG])|([fF][lL][aA])|([sS][wW][fF]))$/;
-    }
-    if (fileType == 'Image2') {
-        reFile = /^.+\.(([jJ][pP][eE]?[gG])|([gG][iI][fF])|([pP][nN][gG]))$/;
+    if (fileType == 'Image') {        
+        reFile = /^.+\.(([jJ][pP][eE]?[gG])|([gG][iI][fF])|([iI][cC][oO])|([pP][dD][fF])|([pP][nN][gG])|([fF][lL][aA])|([sS][wW][fF])|([wW][eE][bB][pP]))$/;
+
     }
 
     if (myValue != "") {
@@ -18,8 +16,6 @@ function FileValidator(myKey, myControl, fileType) {
                 msg = "<b>" + myKey + "</b>&nbsp;&nbsp;&nbsp;Invalid file type.";
             if (fileType == "Image")
                 msg = "<b>" + myKey + "</b>&nbsp;&nbsp;&nbsp;Invalid file type. Valid files: gif, ico, wmv, wma, jpg, jpeg, flv, pdf, png, css, swf, mp3, mp4, rss, txt, mpg, mpeg, avi, wav, aif, aiff, mov, htm, html, fla, xml";
-            if (fileType == "Image2")
-                msg = "<b>" + myKey + "</b>&nbsp;&nbsp;&nbsp;Invalid file type. Valid files: jpg, jpeg, gif, png";
             alert(msg, myControl);
             myControl.focus();
             return false;
